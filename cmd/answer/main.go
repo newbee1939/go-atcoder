23cea65c43ad04b5d7c997c296392eb333c82129
newbee1939 <hideto3970@gmail.com>
2022-03-18 22:54:01 +0900

    仮実装

--- cmd/answer/main.go
+++ cmd/answer/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
 func main() {
-	//
+	n, _ := strconv.Atoi(nextLine())
+	ar := strings.Split(nextLine(), " ")
+
+	c := []int{}
+	for i := 0; i < n; i++ {
+		t, _ := strconv.Atoi(ar[i])
+
+		tmp := 0
+		for t%2 == 0 {
+			t = t / 2
+			tmp++
+		}
+		c = append(c, tmp)
+	}
+	min := c[0]
+	for _, i := range c {
+		if i < min {
+			min = i
+		}
+	}
+	fmt.Println(min)
 }
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// 実装はできたのでリファクタする
