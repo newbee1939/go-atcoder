88fc87b0486c6ae8e288d1ae69ccb917d9c24c97
newbee1939 <hideto3970@gmail.com>
2022-05-02 17:40:18 +0900

    メモの追加

--- cmd/answer/main.go
+++ cmd/answer/main.go
@@ -1,2 +1,3 @@
+// 次に解く問題
 // 問題:https://atcoder.jp/contests/abs/tasks/abc088_b
 // 解答:https://qiita.com/drken/items/fd4e5e3630d0f5859067#5-%E9%81%8E%E5%8E%BB%E5%95%8F%E7%B2%BE%E9%81%B8-10-%E5%95%8F
